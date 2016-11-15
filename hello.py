b049a8aa62c7efbcc62e93a3d1f1d40a9a4598c0
brodyaga <YoTM>
2016-11-15 22:36:10 +0300

    Практикум к 1-9 версия 2.0

--- hello.py
+++ hello.py
@@ -3,11 +3,12 @@
 import cgi, os
 
 def app(environ, start_response):
+
     """ Попытка написать wsgi-приложение"""
     query = os.environ.get('QUERY_STRING')
     print query
     response_headers = [
-        ('Content-type','text/plain')
+        ('Content-type', 'text/plain')
     ]
     start_response(status, response_headers)
     return null
