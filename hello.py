597e1a070aad72214fed21fed47a99db3d5d1bd9
brodyaga <YoTM>
2016-11-15 22:31:54 +0300

    Практикум к 1-9 версия 2.0

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import cgi, os
 
 def app(environ, start_response):
     """ Попытка написать wsgi-приложение"""
     query = os.environ.get('QUERY_STRING')
-    pairs = cgi._parseparam(query)
-    # for key, value in pairs.items():
-    #    print(key, value)
-    status = '200 OK'
+    print query
     response_headers = [
         ('Content-type','text/plain')
     ]
     start_response(status, response_headers)
-    return pairs.items() #iter([data])
+    return
