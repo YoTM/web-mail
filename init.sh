e32790d5ce09afa0be8d9d069823d3dc86c61923
brodyaga <YoTM>
2016-11-16 22:17:32 +0300

    Практикум к 2-1 попытка 1

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# дальше идёт моя попытка автоматизации создания заготовки приложений
+
+sudo django-admin.py ask
+echo "created ask django-proect"
+sudo cd ask
+sudo ./manage.py startapp qa
+echo "created qa app"
