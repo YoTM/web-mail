7bf2d7d4bf961daeda27322e4e89923feaf68d5f
brodyaga <YoTM>
2016-11-16 23:57:12 +0300

    Практикум к 2-1 попытка 1

--- init.sh
+++ init.sh
@@ -2,10 +2,13 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+gunicorn -c /home/box/web/etc/django.py wsgi --daemon
 # дальше идёт моя попытка автоматизации создания заготовки приложений
 
-sudo django-admin.py ask
-echo "created ask django-proect"
+sudo django-admin startproject ask
+echo "created ask django-project"
 sudo cd ask
+sudo chmod +x manage.py
 sudo ./manage.py startapp qa
 echo "created qa app"
