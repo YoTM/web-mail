d4c1f55520fa29c33011e405c1a0fb138cb4bdf2
brodyaga <YoTM>
2016-11-18 00:05:10 +0300

    Практикум к 2-1 попытка 3.2

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-# gunicorn -c /home/box/web/etc/django.py wsgi --daemon
 gunicorn -c /home/box/web/hello.py hello:app --daemon
-gunicorn -c /home/box/web/ask/ask wsgi:application --daemon
+gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application --daemon
