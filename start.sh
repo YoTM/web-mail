d906cb0dc7e980019be857557befaf8d36d3ffc4
brodyaga <YoTM>
2016-11-18 22:11:48 +0300

    Практикум к 2-1 попытка 4.4

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#gunicorn -c /home/box/web/hello.py hello:app --daemon
-#gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application --daemon
-cd /home/box/web
-sudo gunicorn - b 0.0.0.0:8080 hello:app &
-cd /home/box/web/ask/ask
-sudo gunicorn - b 0.0.0.0:8000 wsgi:application &
+gunicorn -c /home/box/web/hello.py hello:app --daemon
+gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application --daemon
+#cd /home/box/web
+#sudo gunicorn -b 0.0.0.0:8000 hello:app &
+#cd /home/box/web/ask/ask
+#sudo gunicorn -b 0.0.0.0:8000 wsgi:application &
