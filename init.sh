3983104c827ab509672ea1b45fc0a0817311a9c1
brodyaga <YoTM>
2016-11-18 23:53:03 +0300

    Практикум к 2-1 попытка 4.8

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
+cd /home/box/web/ask/ask
+sudo gunicorn qa.wsgi &
+
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gunicorn -c /home/box/web/hello.py hello:app --daemon
-gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application --daemon
+#gunicorn -c /home/box/web/hello.py hello:app --daemon
+#gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application --daemon
 #cd /home/box/web
 #sudo gunicorn -b 0.0.0.0:8000 hello:app &
-#cd /home/box/web/ask/ask
-#sudo gunicorn -b 0.0.0.0:8000 wsgi:application &
+cd /home/box/web/ask/ask
+sudo gunicorn qa.wsgi &
