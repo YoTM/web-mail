e0509347db21a6243527a8c1c47500cf22aee69d
brodyaga <YoTM>
2016-11-24 00:05:10 +0300

    Практикум к 2-3 попытка 1.1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Question(models, Model):
+    objects = QuestionManager()
+    title = models.CharField(max_lenght=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForignKey(User)
+    likes = models.ManyToManyField(User)
+
+    class QuestionManager(models.Manager):
+        def new(self):
+                pass
+        def popular(self):
+                pass
+class Answer(models, Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question= models.ForignKey(Question)
+    author = models.ForignKey(User)
+
+
+
