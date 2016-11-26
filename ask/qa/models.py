fb4d81421199ec6f21e0b191d7329f3fa0a021e2
brodyaga <YoTM>
2016-11-26 23:44:46 +0300

    Практикум к 2-3 попытка 1.8

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,14 +14,14 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, related_name='question_author')
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, default='x', related_name='question_author')
     likes = models.ManyToManyField(User, related_name='question_like')
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
 
